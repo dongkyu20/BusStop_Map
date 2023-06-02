76184ea74daa8753ac7c1029a54f8ab01cc3d735
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 19:46:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,17 @@ data = pd.read_csv(csv_file_path1)
 people = pd.read_csv(csv_file_path2)
 
 new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'DATE', 'H_KEY', 'CODE', 'CITY', 'AT']
-new_columns2 = ['loc', 
+new_columns2 = ['LOC', 'COUNT'] 
 df = data.rename(columns=dict(zip(data.columns, new_columns)))
+p_df = data.rename(columns=dict(zip(data.columns, new_columns2)))
 
+people_lst = p_df['LOC'].split()
 
-
-city_lst = list(set(df['city']))
+city_lst = list(set(df['CITY']))
 city_lst.sort()
 
 picked = st.selectbox('Pick City', city_lst)
-picked_city = df['city'] == picked
+picked_city = df['CITY'] == picked
 df_map = df[picked_city]
 st.map(df_map)
 
