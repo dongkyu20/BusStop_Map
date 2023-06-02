1a8174c4bfa3dffcecba26c8bd0eb2c5b90cb188
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 18:26:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 csv_file_path = 'Bus_stop_data.csv'
 
 data = pd.read_csv(csv_file_path)
-pd.DataFrame(data, columns = ['NUM', 'NAM', 'lat', 'lon', 'date', 'h_key', 'code', 'city', 'at'])
+new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'date', 'h_key', 'code', 'city', 'at']
+df = data.rename(columns=dict(zip(data.columns, new_columns)))
 
-st.map(data)
+st.map(df)
