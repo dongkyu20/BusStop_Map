4802520c15c11e23a7ed5f36bf7df7531c9bbb17
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 20:23:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import streamlit as st
 import pandas as pd
 
 csv_file_path1 = 'Bus_stop_data.csv'
+csv_file_path2 = 'people_re.csv'
 
 
 data = pd.read_csv(csv_file_path1)
+people = pd.read_csv(csv_file_path2, encoding='EUC-KR')
 
 
 new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'DATE', 'H_KEY', 'CODE', 'CITY', 'AT']
+new_columns2 = ['LOC', 'COUNT']
+
 
 df = data.rename(columns=dict(zip(data.columns, new_columns)))
+p_df = people.rename(columns=dict(zip(people.columns, new_columns2)))
+
+
 
 
 
