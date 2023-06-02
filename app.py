166b3f212865caa0dd0b4cb5c52fa51c5699bcf9
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 18:33:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ data = pd.read_csv(csv_file_path)
 new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'date', 'h_key', 'code', 'city', 'at']
 df = data.rename(columns=dict(zip(data.columns, new_columns)))
 
+
+st.text_area('South Korea')
+dd = st.selectbox('pick city', ['아산시', '파주시'])
 st.map(df)
