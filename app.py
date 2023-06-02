b847fc01511a0db9fa75903fc6fbf6e2e598b34a
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 18:09:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import pandas as pd
 csv_file_path = 'Bus_stop_data.csv'
 
 data = pd.read_csv(csv_file_path)
-
+pd.DataFrame(data, columns = ['NUM', 'NAM', 'lat', 'lon', 'date', 'h_key', 'code', 'city', 'at'])
 
 st.map(data)
