21f609f07b9a0e0009b163f6cb6268b5b6843920
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 18:54:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ data = pd.read_csv(csv_file_path)
 new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'date', 'h_key', 'code', 'city', 'at']
 df = data.rename(columns=dict(zip(data.columns, new_columns)))
 
+df = list(set(df['NAME']))
 
 city_lst = list(set(df['city']))
 city_lst.sort()
 
-st.text_area('South Korea')
 picked = st.selectbox('Pick City', city_lst)
 picked_city = df['city'] == picked
 df_map = df[picked_city]
 st.map(df_map)
+
+st.text_area('South Korea')
+
