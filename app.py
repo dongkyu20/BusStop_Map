5c280cea1531ba9e2dc3a1ed0c966889f7a839fc
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-06-02 20:03:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import streamlit as st
 import pandas as pd
 
 csv_file_path1 = 'Bus_stop_data.csv'
-csv_file_path2 = 'people.csv'
+
 
 data = pd.read_csv(csv_file_path1)
-people = pd.read_csv(csv_file_path2)
+
 
 new_columns = ['NUM', 'NAME', 'LAT', 'LON', 'DATE', 'H_KEY', 'CODE', 'CITY', 'AT']
-new_columns2 = ['LOC', 'COUNT'] 
-df = data.rename(columns=dict(zip(data.columns, new_columns)))
-p_df = data.rename(columns=dict(zip(data.columns, new_columns2)))
 
-people_lst = p_df['LOC'].split()
+df = data.rename(columns=dict(zip(data.columns, new_columns)))
+
+
 
 city_lst = list(set(df['CITY']))
 city_lst.sort()
